b22e77b5d954770bc6b560419fe7ef070b5ff6bc
kamakship18 <kamakshipandoh8@gmail.com>
2023-12-09 12:56:20 +0530

    updated mobile.js

--- src/mobile.js
+++ src/mobile.js
@@ -1,15 +1,85 @@
 // Progression 1 SmartPhone
 
-class SmartPhone {}
-
+class SmartPhone {
+    constructor(ram, batteryPower, price) {
+      this.ram = ram;
+      this.batteryPower = batteryPower;
+      this.price = price;
+    }
+    displayFeatures() {
+      return this.ram + '\n' + this.batteryPower + '\n' + this.price;
+    }
+    remainingAmount(priceIHave) {
+      this.price = this.price - priceIHave;
+    }
+  }
+  
 // Progression 2 AndroidPhone
 
-class AndroidPhone extends SmartPhone {}
+class AndroidPhone extends SmartPhone {
+    constructor(modelName, ram, batteryPower, price) {
+      super(ram, batteryPower, price);
+      this.modelName = modelName;
+    }
+  
+    displayFeatures() {
+      return (this.modelName +'\n' +this.ram + '\n' +this.batteryPower +'\n' + this.price );
+    }
+    remainingAmount(priceIHave) {
+      this.price = priceIHave - this.price;
+      if (this.price <= 0){
+        return 'Customer has no more amount'
+      }else{
+         return `Customer has remaining Rs.${this.price} after buying the android phone`;
+       }
+      }
+    }
 
 // Progression 3 IPhone
 
-class IPhone extends SmartPhone {}
+class IPhone extends SmartPhone {
+    constructor(seriesName, ram, batteryPower, price) {
+      super(ram, batteryPower, price);
+      this.seriesName = seriesName;
+    }
+  
+    displayFeatures() {
+      return (
+        this.seriesName +'\n' +this.ram +'\n' +this.batteryPower +'\n' +this.price );
+    }
+  
+    remainingAmount(priceIHave) {
+      const initialPrice = this.price;
+      this.price = priceIHave - this.price;
+      if (initialPrice > priceIHave){
+        return "Customer can't able to buy a phone due to insufficient amount"
+       }else if(this.price <= 0){
+        return 'Customer has no more amount';
+       }else{
+        return 'Customer has remaining Rs.' + this.price + ' after buying an iphone';
+       }
+    }
+  }
+  
 
 // Progression 4  Progression 1 MobileCampus
 
-class MobileCampus {}
+class MobileCampus {
+    constructor(NoOfMobiles, NoOfAndroidMobiles, NoOfIPhoneMobiles) {
+        this.NoOfMobiles = NoOfMobiles;
+        this.NoOfAndroidMobiles = NoOfAndroidMobiles;
+        this.NoOfIPhoneMobiles = NoOfIPhoneMobiles;
+      }
+    
+      changeMobileAvailabilityNumber(number, mobileType) {
+        this.NoOfMobiles -= number;
+        if (mobileType == 'android') {
+          this.NoOfAndroidMobiles = this.NoOfAndroidMobiles - number;
+          return 'Available android mobiles are ' + this.NoOfAndroidMobiles;
+        }
+        if (mobileType = 'iphone') {
+          this.NoOfIPhoneMobiles = this.NoOfIPhoneMobiles - number;
+          return 'Available iphone mobiles are ' + this.NoOfIPhoneMobiles;
+        }
+      }
+    }
